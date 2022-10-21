5ec73de54074f2d398b9cd445f51f45f39e4b64d
会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>
2022-10-21 00:41:16 +0000

    上传
    Signed-off-by: 会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>

---
+++ apps/chatterboxStat.js
@@ -0,0 +1,75 @@
+import { segment } from "oicq";
+import fetch from "node-fetch";
+const _path = process.cwd();
+let ing = {};
+export const rule = {
+    FuckingChatterbox: {
+        reg: "^#*(话痨统计|话痨检测|水逼检测|水逼统计)$", 
+        priority: 60,
+        describe: "寻找大水逼",
+    },
+};
+export async function FuckingChatterbox(e) {
+    if(ing[e.group_id]){
+        e.reply("在找了，在找了，已经在找了，很慢，请再等等！");
+        return true;
+    }
+    ing[e.group_id] = 1;
+    e.reply("正在分析聊天记录，寻找本群大水逼，请等一等！");
+    let CharHistory = await e.group.getChatHistory(0, 1);
+    let seq = CharHistory[0].seq;
+    e.reply(`大概有${seq}条记录需要分析，预计需要${(seq/20/4/60).toFixed(2)}分钟`);
+    let CharList = {};
+    let allcount = 0;
+    console.log("seq:",seq);
+    for (let i = seq; i > 0; i = i - 20) {
+        let CharTemp = await e.group.getChatHistory(i, 20);
+        if (CharTemp.length == 0) {
+            break;
+        }
+        for (const key in CharTemp) {
+            if (CharTemp[key].length == 0) {
+                continue;
+            }
+            if(CharTemp[key].user_id == BotConfig.account.qq){
+                continue;
+            }
+            allcount ++;
+            if (CharList[CharTemp[key].user_id]) {
+                CharList[CharTemp[key].user_id].times += 1;
+                CharList[CharTemp[key].user_id].uname = CharTemp[key].sender.card ? CharTemp[key].sender.card : CharTemp[key].sender.nickname;
+            } else {
+                CharList[CharTemp[key].user_id] = {
+                    times: 1,
+                    user_id: CharTemp[key].user_id,
+                    uname: CharTemp[key].sender.card ? CharTemp[key].sender.card : CharTemp[key].sender.nickname
+                };
+            }
+        }
+    }
+    let CharArray = [];
+    for (const key in CharList) {
+        CharArray.push(CharList[key]);
+    }
+    CharArray.sort((a, b) => {
+        return b.times - a.times
+    })
+    console.log(CharArray);
+    let l = Math.ceil(CharArray.length / 10);
+    CharArray = CharArray.slice(0, l > 10 ? 10 : l);
+    let res = `一共检测到聊天记录 ${allcount} 句话，其中：\n`;
+    let itemp = 0;
+    for (let v of CharArray) {
+        res += (v.uname) + `  水了${v.times}句话，占比${(v.times/allcount*100).toFixed(2)}% `;
+        if(!itemp){
+            res += " （这人水是真滴多 ！）";
+        }
+        if(itemp != CharArray.length -1 ){
+            res += "\n";
+        }
+        itemp++;
+    }
+    e.reply(res);
+    ing[e.group_id] = 0;
+    return true;
+}
