6e59904dce3c9ec69b4e27182d60b1472eb4a25e
会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>
2022-11-13 09:51:10 +0000

    增加新的功能
    Signed-off-by: 会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>

---
+++ apps/群友强制休息.js
@@ -0,0 +1,43 @@
+import {
+    segment
+} from "oicq";
+
+const _path = process.cwd();
+export const rule = {
+    examples: {
+        reg: "^#我要休息[\s\S]*", //匹配消息正则，命令正则
+        priority: 750, //优先级，越小优先度越高
+        describe: "#我要休息XX分钟 || 天 || 小时", //【命令】功能说明
+    },
+};
+
+export async function examples(e) {
+    // console.log(e.msg);
+    console.log(e.user_id);
+    let who = e.user_id
+    let val = e.msg.slice(5);
+    let day = val.replace(/[0-9]*/g, '').replace(/\./, '');
+    let num = parseInt(val);
+    let timer = num * 60
+    try {
+        switch (day) {
+            case '天':
+                timer = timer * 60 * 24
+                break;
+            case '小时':
+                timer = timer * 60
+                break;
+            default:
+                break;
+        }
+        console.log(timer);
+        e.group.muteMember(who, timer);
+        let msg = [
+            segment.at(who), "\n",
+            `没有后悔药的喔!!!请等待${num}${day}后再聊天`
+        ]
+        e.reply(msg);
+    } catch (error) {
+        e.reply(`请检查是否机器人有禁言权限!该功能仅限群聊使用!`);
+    }
+}
