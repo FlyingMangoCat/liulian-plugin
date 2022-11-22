b47d66913b11b7d77852f05505a7cac1a6d0e485
会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>
2022-11-22 09:27:38 +0000

    更改地址
    Signed-off-by: 会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>

---
+++ guoba.support.js
@@ -0,0 +1,25 @@
+import path from "path";
+const _path = process.cwd() + "/plugins/liulian-plugin";
+
+/**
+ *  支持锅巴配置
+ */
+export function supportGuoba() {
+  return {
+    pluginInfo: {
+      name: "liulian-plugin",
+      title: "榴莲插件（liulian-plugin）",
+      author: "@会飞的芒果猫",
+      authorLink: "https://gitee.com/huifeidemangguomao",
+      link: "https://gitee.com/huifeidemangguomao/liulian-plugin",
+      isV3: true,
+      isV2: true,
+      description: "提供了一些群聊功能",
+      // 显示图标，此为个性化配置
+      // 图标可在 https://icon-sets.iconify.design 这里进行搜索
+      icon: "",
+      // 图标颜色，例：#FF0000 或 rgb(255, 0, 0)
+      iconColor: "#7CFC00",
+    },
+  };
+}
