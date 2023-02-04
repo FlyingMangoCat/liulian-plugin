d476fd146458eeae7c09b70ec9c8e603792c50c4
会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>
2023-02-04 07:58:06 +0000

    添加开关
    Signed-off-by: 会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>

---
+++ apps/打卡(加好友私聊打卡还能点赞).js
@@ -0,0 +1,40 @@
+import { segment } from "oicq";
+import fetch from "node-fetch";
+import Cfg from '../components/Cfg.js'
+//项目路径
+const _path = process.cwd();
+export const rule = {
+    
+    chumeng: {
+        reg: "打卡$", //匹配消息正则，命令正则
+        priority: 100, //优先级，越小优先度越高
+        describe: "打卡or点赞", //【命令】功能说明
+    },
+};
+
+export async function chumeng(e) {
+if (!/榴莲/.test(e.msg) && !Cfg.get('sys.dk', false)) {
+    return false
+  }
+    console.log("用户命令：", e.msg);
+
+    let url = `https://api.iyk0.com/ecy/api.php`;
+    let msg = [
+       "打卡成功!可心成功帮你点赞!",
+        segment.image(url),
+    ];
+   let qun = [
+       segment.at(e.user_id),
+       " 打卡成功!这是给你的奖励!",
+        segment.image(url),
+       "\nps:点赞要加好友私聊:打卡",
+    ];
+   let type = e.message_type;
+    if (type == 'private') {   //私聊
+        e.friend.thumbUp(e.user_id,20);
+        e.reply(msg);
+    } else if (type == 'group') {    //群聊
+    e.group.sendMsg(qun)
+    }
+    return true;//返回true 阻挡消息不再往下
+}
