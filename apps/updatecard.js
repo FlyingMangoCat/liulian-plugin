8e59876ebf3db3fdf5a610f7985d2d30f94429af
会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>
2023-02-04 03:52:36 +0000

    部分功能添加开关
    Signed-off-by: 会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>

---
+++ apps/updatecard.js
@@ -0,0 +1,43 @@
+import os from 'os';
+import schedule from "node-schedule";
+import Cfg from '../components/Cfg.js'
+
+let botname = ''//这里改成bot的名字
+
+
+schedule.scheduleJob("0 0/10 * * * ?", async () =>{
+    const totalMem = os.totalmem();
+    const freeMen =os.freemem();
+    let persent = (totalMem-freeMen)/totalMem*100;
+    console.log(persent.toFixed(2));
+    for(let group of Bot.gl){
+        await Bot.pickGroup(group[0]).setCard(Bot.uin, `${botname || Bot.nickname}｜系统占用${persent.toFixed(2)}%`);
+    }
+    return true;
+})
+
+
+export const rule = {
+    qmp : {
+        reg : "^更新群名片",
+        priority: 10,
+        describe : "",
+    }
+};
+
+export async function qmp (e){
+if (!/榴莲/.test(e.msg) && !Cfg.get('sys.qmp', false)) {
+    return false
+  }
+    const totalMem = os.totalmem();
+    const freeMen =os.freemem();
+
+    let persent = (totalMem-freeMen)/totalMem*100;
+
+    console.log(persent.toFixed(2));
+
+    for(let group of Bot.gl){
+        await Bot.pickGroup(group[0]).setCard(Bot.uin, `${botname}|系统占用${persent.toFixed(2)}%`);
+    }
+    return true;
+}
