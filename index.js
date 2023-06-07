e56875c3c0b86b1dc528169a4b97cccf952365c8
会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>
2023-06-07 17:24:18 +0000

    简化
    Signed-off-by: 会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>

---
+++ index.js
@@ -0,0 +1,28 @@
+import { isV3 } from './components/Changelog.js'
+import Data from './components/Data.js'
+import config from './model/config/config.js'
+import chalk from 'chalk'
+
+export * from './apps/index.js'
+
+let index = { liulian: {} }
+if (isV3) {
+  index = await Data.importModule('/plugins/liulian-plugin/adapter', 'index.js')
+}
+
+export const liulian = index.liulian || {}
+
+const plugininfo_default = config.getdefault_config('liulian', 'plugininfo')
+
+await redis.set('Yz:liulian:config:pluginname', plugininfo_default.pluginname)
+await redis.set('Yz:liulian:config:version', plugininfo_default.version)
+await redis.set('Yz:liulian:config:author', plugininfo_default.author)
+let currentplugininfo = await config.getcurrentplugininfo()
+
+logger.info(`~~~~~~~~~~~ ^_^ ~~~~~~~~~~~`)
+logger.info(`~\t${chalk.yellow('欢迎使用' + currentplugininfo.pluginname + '插件')}`)
+logger.info(`~\t${chalk.green('版本：')}\t${chalk.blue(currentplugininfo.version)}`)
+logger.info(`~\t${chalk.green('作者：')}${chalk.greenBright(currentplugininfo.author)}`)
+logger.info(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`)
+
+console.log(`\t${chalk.greenBright('榴莲插件' + currentplugininfo.version + '初始化~')}`)
