c4d0b4571ad5fb86c154919c019de003127437ff
会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>
2023-06-17 08:10:56 +0000

    调整引用
    Signed-off-by: 会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>

---
+++ components/index.js
@@ -0,0 +1,7 @@
+import Data from './Data.js'
+import Common from './Common.js'
+import Cfg from './Cfg.js'
+import Data2 from './Data2.js'
+import Version from './Version.js'
+
+export { Cfg, Common, Data, Data2,Version, }
