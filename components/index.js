30e3e45c71e76a583f0d85307abaa041aba094ed
会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>
2023-07-01 02:01:07 +0000

    删除文件 components/index.js

--- components/index.js
+++
@@ -1,7 +0,0 @@
-import Data from './Data.js'
-import Common from './Common.js'
-import Cfg from './Cfg.js'
-import Data2 from './Data2.js'
-import Version from './Version.js'
-
-export { Cfg, Common, Data, Data2, Version, }
