79ea8721731d9674ec5500cc14b6a6c7e80173af
会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>
2023-07-01 02:01:58 +0000

    修复`B站推送`
    Signed-off-by: 会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import Data from './Data.js'
+import Common from './Common.js'
+import Cfg from './Cfg.js'
+import Data2 from './Data2.js'
+import Version from './Version.js'
+import { getBLsid, getUuid } from './ckMaker.js'
+
+export { Cfg, Common, Data, Data2, Version, getBLsid, getUuid }
