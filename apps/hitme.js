937105753b28291a986dead9789f73e58403472d
会飞的芒果猫 <naguodundae@126.com>
2024-03-23 05:00:15 +0000

    修复
    Signed-off-by: 会飞的芒果猫 <naguodundae@126.com>

---
+++ apps/hitme.js
@@ -0,0 +1,53 @@
+import Cfg from '../components/Cfg.js'
+import config from "../model/config/config.js"
+const cfg = config.getdefault_config('liulian', 'botname', 'config');
+  const botname = cfg.botname
+const _path = process.cwd();
+
+let hit = ['一拳!','两拳!', '三拳!', '四拳!', '五拳!', '六拳!', '七拳!', '八拳!', '九拳!', '十拳!'];
+let MuteTime = 60;
+export const rule = {
+    HitMe: {
+        reg: "#打我", //匹配消息正则，命令正则
+        priority: 1000, //优先级，越小优先度越高
+        describe: "【#禁言自己1-10次】", //【命令】功能说明
+    },
+
+};
+
+export async function HitMe(e) {
+if (!Cfg.get('sys.dw', false)) {
+    e.reply (`该功能已被关闭，请通过留恋设置开启`);
+    return false
+  }
+	let random = Math.round(Math.random() * 9);//随机生成 0-9 
+	let msg;
+ 	msg = [
+   		segment.at(e.user_id),
+    	 	`\n今天的人品是【`,
+		 (random+1).toString(),
+		`】，让${botname}满足你！`
+  	];
+  	
+  	e.reply(msg); //发送消息
+  	await sleep(2000) //延时
+	
+    for (let i = 0; i <= random; i++) {
+  		e.reply(hit[i]);
+		e.group.muteMember(e.user_id, MuteTime*(i+1)); 
+		await sleep(2000); 
+	} 
+	
+        
+        e.reply (`够了吗！`); //发送消息
+
+        return true; //返回true 阻挡消息不再往下
+
+   
+}
+
+
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
---
+++ apps/伪造信息.js
@@ -0,0 +1,94 @@
+import fetch from "node-fetch";
+import lodash from "lodash";
+import Cfg from '../components/Cfg.js'
+
+let list = [1280951594,3598537042]; //禁止伪造的qq放到这里
+
+export const rule = {
+  forge: {
+    reg: "^#(伪造信息|伪造消息).*$", //匹配消息正则，命令正则
+    priority: 50, //优先级，越小优先度越高
+    describe: "#伪造信息@群成员 信息", 
+  },
+};
+
+export async function forge(e) {
+if (!Cfg.get('sys.forge', false)) {
+    e.reply (`该功能已被关闭，请通过留恋设置开启`);
+    return false
+  }
+  let msgInfo = new Map(); 
+  let msg = []; 
+  let idx = 1;
+
+  let qqList = new Set();
+
+  let tempAtInfo = {};
+  for (let index = 0; index < e.message.length; index++) {
+    let element = e.message[index];
+    if (element.text) {
+      element.text = element.text.replace(/#伪造信息/g, "").trim();
+    }
+
+    if (element.type == "at") {
+      const key = idx;
+      msgInfo.set(key, {
+        qq: element.qq,
+        name: element.text.replace(/@/g, ""),
+        msg: "",
+      });
+      tempAtInfo = {
+        key,
+        qq: element.qq,
+        name: element.text.replace(/@/g, ""),
+        msg: "",
+      };
+      idx++;
+      qqList.add(element.qq);
+    }
+    if (element.type == "text" && element.text && tempAtInfo.key) {
+      msgInfo.set(tempAtInfo.key, {
+        qq: tempAtInfo.qq,
+        name: tempAtInfo.name,
+        msg: element.text,
+        type: element.type,
+      });
+    }
+
+    if (element.type == "image" && element.url && tempAtInfo.key) {
+      msgInfo.set(tempAtInfo.key, {
+        qq: tempAtInfo.qq,
+        name: tempAtInfo.name,
+        msg: element.text,
+        type: element.type,
+        url: element.url,
+      });
+    }
+  }
+
+  if (lodash.intersection(list, Array.from(qqList)).length > 0) {
+    e.reply("这位大人也是你配造谣的？");
+    return true;
+  }
+
+  for (let item of msgInfo.keys()) {
+    const tempMsg = msgInfo.get(item);
+    let tempMsgList = [];
+    if (tempMsg.type === "image") {
+      tempMsgList = [segment.image(tempMsg.url)];
+    } else {
+      tempMsgList = tempMsg.msg.split(/[|｜]/); 
+    }
+    tempMsgList.map((msgItem) => {
+      msg.push({
+        message: msgItem,
+        nickname: tempMsg.name,
+        user_id: tempMsg.qq,
+      });
+    });
+  }
+
+  e.reply(await e.group.makeForwardMsg(msg));
+
+  return true; //返回true 阻挡消息不再往下
+}
