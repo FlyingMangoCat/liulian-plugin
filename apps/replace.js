0e1a1110972e63016afa4bfa10f425c69daf996b
会飞的芒果猫 <naguodundae@126.com>
2024-10-01 10:36:35 +0000

    Revert "删除文件 apps/replace.js"
    This reverts commit ae48527a5806c24cf5ec67472bc0e0970cb7bc0b.

---
+++ apps/replace.js
@@ -0,0 +1,18 @@
+const _path = process.cwd();
+const replace_list = {
+  "^/": "#",
+}
+export const rule = {
+  replace: {
+    reg: "",
+    priority: -10,
+    describe: "",
+  }
+};
+export async function replace(e){
+    for (const key in replace_list) try {
+      const reg = RegExp(key)
+      if (!reg.test(e.msg)) continue
+      e.msg = e.msg.replace(reg, replace_list[key])
+    } catch (err) {}
+}
