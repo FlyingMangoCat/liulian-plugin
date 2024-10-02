f6e8aa1c2f2c8f33248c1b754865c5f72a66a452
会飞的芒果猫 <naguodundae@126.com>
2024-10-02 14:40:34 +0000

    help
    Signed-off-by: 会飞的芒果猫 <naguodundae@126.com>

--- apps/help.js
+++ apps/help.js
@@ -1,6 +1,6 @@
 import {help, versionInfo } from './help/help.js'
 import {bilibilihelp, YZversionInfo } from './help/bilibilihelp.js'
-import {maphelp, mapnumber } from './help/map.js'
+import {maphelp, mapnumber } from './help/maphelp.js'
 import {pluginhelp } from './help/pluginhelp.js'
 import { App } from '#liulian'
 
