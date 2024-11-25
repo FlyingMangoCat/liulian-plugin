760ef6e295dc0a1d175f146cdb5c4383dbcdb4a3
会飞的芒果猫 <naguodundae@126.com>
2024-11-25 01:32:49 +0000

    乐子图相关更新
    Signed-off-by: 会飞的芒果猫 <naguodundae@126.com>

--- apps/classic.js
+++ apps/classic.js
@@ -11,6 +11,16 @@ export const rule = {
     priority: 1000, //优先级，越小优先度越高
     describe: "经典发言", //【命令】功能说明
   }, 
+  allclassic: {
+    reg: "#?查看全部乐子人$", //匹配消息正则，命令正则
+    priority: 1000, //优先级，越小优先度越高
+    describe: "经典发言", //【命令】功能说明
+  }, 
+  bmclassic: {
+    reg: "#?查看(.*)别名$", //匹配消息正则，命令正则
+    priority: 1000, //优先级，越小优先度越高
+    describe: "经典发言", //【命令】功能说明
+  }, 
 };
 
 export async function sjclassic(e) {
@@ -23,9 +33,32 @@ export async function sjclassic(e) {
 
   e.reply(msg,true);
   }
-  export async function zdclassic(e) {
+export async function zdclassic(e) {
 let keyword = e.msg.replace("#","");
   keyword = keyword.replace("随机","");
+  console.log(keyword);//控制台输出关键词
+  let url = `https://api.yxyos.com/liulian/classic/?list=${keyword}&type=alias`;//请求api
+let response = await fetch(url);
+ let res = await response.json();//结果json字符串转对象
+       let msg = [
+           segment.image(res.url)
+  ];
+
+  e.reply(msg,true);
+  }
+export async function allclassic(e) {
+ let url = `https://api.yxyos.com/liulian/classic/?list=all`;//请求api
+ let response = await fetch(url);
+ let res = await response.json();//结果json字符串转对象
+       let msg = [
+           segment.image(res.resources)
+  ];
+
+  e.reply(msg,true);
+  }
+export async function bmclassic(e) {
+let keyword = e.msg.replace("#","");
+  keyword = keyword.replace("查看","");
   console.log(keyword);//控制台输出关键词
   let url = `https://api.yxyos.com/liulian/classic/?list=${keyword}&type=alias`;//请求api
 let response = await fetch(url);
