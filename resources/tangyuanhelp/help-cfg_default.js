81f90086beaa3e9b912760e18785d16ca07427c8
会飞的芒果猫 <naguodundae@126.com>
2026-03-09 00:01:59 +0800

    v0.13.13 - 新增汤圆帮助系统，优化帮助分类

--- resources/tangyuanhelp/help-cfg_default.js
+++ resources/tangyuanhelp/help-cfg_default.js
@@ -15,19 +15,151 @@ export const helpCfg = {
 
 // 帮助菜单内容
 export const helpList = [{
-  group: "小功能合集",
+  group: "群聊互动",
+  list: [{
+    icon: 47,
+    title: "娶群友",
+    desc: "根据发言时间抽群友老婆"
+  }, {
+    icon: 18,
+    title: "姬霓太美 菜虚鲲",
+    desc: "开团"
+  }, {
+    icon: 35,
+    title: "打卡",
+    desc: "私聊发送自动点赞"
+  }, {
+    icon: 75,
+    title: "#伪造信息@群友+内容",
+    desc: "指定群友伪造信息"
+  }, {
+    icon: 98,
+    title: "#我要休息",
+    desc: "休息xx分钟/小时/天"
+  }, {
+    icon: 90,
+    title: "#广播",
+    desc: "多群广播信息"
+  }, {
+    icon: 59,
+    title: "随机表情",
+    desc: "群聊/私聊均可用"
+  }]
+}, {
+  group: "娱乐游戏",
   list: [{
     icon: 78,
     title: "#猜谜语",
-    desc: "随机谜语，30秒后公布答案，回答格式：#谜底[答案]"
+    desc: "随机谜语，回答格式：#谜底[答案]"
+  }, {
+    icon: 74,
+    title: "#猜歌名",
+    desc: "猜歌曲名称"
+  }, {
+    icon: 78,
+    title: "#猜角色",
+    desc: "支持原神/星铁/邦布"
+  }, {
+    icon: 71,
+    title: "#芒果问答",
+    desc: "原神知识问答"
+  }, {
+    icon: 44,
+    title: "猫猫系统",
+    desc: "抱走猫猫、猫猫突袭"
+  }]
+}, {
+  group: "生活信息",
+  list: [{
+    icon: 50,
+    title: "#星座运势",
+    desc: "查看星座运势"
+  }, {
+    icon: 93,
+    title: "#运势",
+    desc: "查看今日运势"
+  }, {
+    icon: 52,
+    title: "#早报",
+    desc: "当天新闻早报"
+  }, {
+    icon: 100,
+    title: "#天气",
+    desc: "查看指定城市天气"
   }, {
     icon: 67,
     title: "#历史上的今天",
-    desc: "查看历史上今天发生的事件"
+    desc: "查看历史事件"
+  }, {
+    icon: 11,
+    title: "#话痨统计",
+    desc: "统计群里话痨"
+  }]
+}, {
+  group: "趣味功能",
+  list: [{
+    icon: 43,
+    title: "#毒鸡汤",
+    desc: "随机毒鸡汤"
+  }, {
+    icon: 54,
+    title: "#土味情话",
+    desc: "说点土味情话"
+  }, {
+    icon: 94,
+    title: "#讲个笑话",
+    desc: "讲个笑话"
+  }, {
+    icon: 30,
+    title: "#舔狗日记",
+    desc: "随机舔狗日记"
+  }, {
+    icon: 60,
+    title: "#发病",
+    desc: "发病文学"
   }, {
     icon: 98,
-    title: "#翻译[内容]",
+    title: "#翻译",
     desc: "翻译指定内容"
+  }, {
+    icon: 22,
+    title: "#每日单词/句子",
+    desc: "练习英语"
+  }]
+}, {
+  group: "测试娱乐",
+  list: [{
+    icon: 56,
+    title: "#神之眼",
+    desc: "查看神之眼"
+  }, {
+    icon: 42,
+    title: "#打我",
+    desc: "根据人品禁言"
+  }, {
+    icon: 100,
+    title: "#经典语录",
+    desc: "经典小怪话"
+  }, {
+    icon: 100,
+    title: "#随机[内容]",
+    desc: "随机图片"
+  }, {
+    icon: 95,
+    title: "#二次元的我",
+    desc: "查看二次元属性"
+  }, {
+    icon: 96,
+    title: "#我的成分",
+    desc: "查看成分"
+  }, {
+    icon: 97,
+    title: "#答案之书",
+    desc: "答案之书"
+  }, {
+    icon: 99,
+    title: "#观音灵签",
+    desc: "观音灵签"
   }]
 }, {
   group: "其他帮助",
